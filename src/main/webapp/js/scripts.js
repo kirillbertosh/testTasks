5994ef075af2ac2d46c6ae9a9a7c7eac05970f74
Kirill Bertosh <kirillbertosh@gmail.com>
2018-03-13 00:10:29 +0300

    move scripts to separate file

---
+++ src/main/webapp/js/scripts.js
@@ -0,0 +1,34 @@
+function sendLoginData() {
+    function toJSONString( form ) {
+        var obj = {};
+        var elements = form.querySelectorAll( "input, select, textarea" );
+        for( var i = 0; i < elements.length; ++i ) {
+            var element = elements[i];
+            var name = element.name;
+            var value = element.value;
+
+            if( name ) {
+                obj[ name ] = value;
+            }
+        }
+        console.log(obj);
+        return JSON.stringify( obj );
+    }
+
+    document.addEventListener( "DOMContentLoaded", function() {
+        var form = document.getElementById( "test" );
+        var output = document.getElementById( "output" );
+        form.addEventListener( "submit", function( e ) {
+            e.preventDefault();
+            var json = toJSONString( this );
+            output.innerHTML = json;
+            console.log(json);
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'http://localhost:8081/api/v1/users', true);
+            xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            xhr.setRequestHeader('Accept', 'application/json -d');
+            xhr.send(json);
+        }, false);
+
+    });
+};
